07f8c7e0176112c216b5821da90ba7d7925edf77
yanmingxing <admin123456>
2022-08-29 19:43:08 +0800

    单机版Eureka-server

---
+++ cloud-eureka-server7001/src/main/java/com/yss/springcloud/EurekaServerMain7001.java
@@ -0,0 +1,18 @@
+package com.yss.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: ymx
+ * @date: 2022/8/29
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerMain7001.class,args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/yss/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yss/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.yss.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: ymx
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description: 启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
