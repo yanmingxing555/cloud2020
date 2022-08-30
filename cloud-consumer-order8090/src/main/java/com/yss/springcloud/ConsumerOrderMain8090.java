20a04a2b55091d821c09948a64cbea1bc1f852d2
yanmingxing <admin123456>
2022-08-30 19:28:56 +0800

    8-1、Ribbon负载均衡服务调用

--- cloud-consumer-order8090/src/main/java/com/yss/springcloud/ConsumerOrderMain8090.java
+++ cloud-consumer-order8090/src/main/java/com/yss/springcloud/ConsumerOrderMain8090.java
@@ -1,7 +1,10 @@
 package com.yss.springcloud;
 
+import com.yss.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author: ymx
@@ -9,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0.0.0
  */
 @SpringBootApplication
+@EnableEurekaClient
+//在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效，形如
+@RibbonClient(name = "cloud-payment-service",configuration = MySelfRule.class)
 public class ConsumerOrderMain8090 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrderMain8090.class,args);
